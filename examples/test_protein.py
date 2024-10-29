4cab4869140568e39290fa979fcef080b2df2b27
cmiller-veced <cary.miller66@gmail.com>
2024-10-29 09:40:09 -0600

    mv

--- examples/test_protein.py
+++ examples/test_protein.py
@@ -1,48 +1,6 @@
-from apix.tools import (raw_swagger, LocalValidationError,)
-from apix.dyno import (dynamic_validator, dynamic_call,)
-
-from info import local
-
-class ValidDataBadResponse(LocalValidationError): pass
-
-class NonTruthy(LocalValidationError): pass
-
-class InvalidAccessionId(LocalValidationError): pass
-
-
-def local_validate(params):
-    """Catch data problems missed by the schema.
-    """
-    if not params:
-        raise NonTruthy(params)
-    if params == {'accession': 'xxxxxxxx'}:
-        raise InvalidAccessionId(params)
-
-
-def altered_raw_swagger(jdoc):
-    """Alter raw data to conform with local code assumptions.
-    """
-    patch = dict(parameters=[])
-    jdoc['paths']['/das/s4entry']['get'].update(patch)
-    jdoc['paths']['/']['get'].update(patch)
-    return jdoc
-
-
-class config:
-    swagger_path = local.swagger.protein
-    api_base = local.api_base.protein
-    alt_swagger = altered_raw_swagger
-    head_func = lambda endpoint, verb: {}
-    validate = local_validate
-
-
-_validator = dynamic_validator(config)
-call = dynamic_call(config)
-
-
-# test test test test test test test test test test test test test test test
-##############################################################################
 from collections import defaultdict
+
+from apis.protein import _validator, call, altered_raw_swagger
 
 from test_data_protein import test_parameters
 
