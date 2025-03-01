8282dd0287f94013b26adafd2ac0b894390331a4
Cary Miller <cary.miller66@gmail.com>
2025-02-28 17:03:56 -0700

    tidy up

--- pyapix/osdu/crs_catalog_api.py
+++ pyapix/osdu/crs_catalog_api.py
@@ -2,7 +2,6 @@ from datetime import datetime
 
 from pyapix.tool import api_tools
 from pyapix.tool.api_tools import (dynamic_validator, dynamic_call, SurpriseArgs)
-#from pyapix.tool.api_tools import *
 from pyapix.tool.tools import (LocalValidationError, ValidDataBadResponse, )
 from pyapix.client.info import local
 
@@ -27,7 +26,7 @@ class config:
     validate = local_validate
 
 
-from pyapix.tool.tools import parsed_file_or_url, list_of_dict_to_dict
+from pyapix.tool.tools import parsed_file_or_url
 from pyapix.tool.api_tools import endpoints_ands_verbs
 ends = endpoints_ands_verbs(parsed_file_or_url(config.swagger_path))
 _validator = dynamic_validator(config)
@@ -39,10 +38,18 @@ call = dynamic_call(config)
 
 import json
 
+from pyapix.tool.api_tools import endpoints_ands_verbs
+from pyapix.tool.tools import parsed_file_or_url, list_of_dict_to_dict
 from pyapix.tool.working_with_postman import fetch_thing, insert_params
 from pyapix.tool.do_postman_osdu import pm_files
 import crs_conversion_api as con
 
+one_dict = list_of_dict_to_dict()
+
+# TODO: mv this...
+service_parts = ['api', 'crs', 'catalog', 'conversion', 'search', 'storage',
+                 'legal'
+]
 
 # TODO: mv to do_postman_osdu
 def is_version(word):
@@ -67,14 +74,7 @@ def is_bad_schema(schema):
     return False
 
 
-one_dict = list_of_dict_to_dict()
-
-service_parts = ['api', 'crs', 'catalog', 'conversion', 'search', 'storage',
-                 'legal'
-]
-
-# Fill in some missing functionality in tool.tools.
-
+# TODO: rename to test_endpoints_ands_verbs    ??????
 def test_inspect_swagger():
     """
     CRS Catalog
@@ -87,20 +87,35 @@ def test_inspect_swagger():
     for (e, v) in evs:
         print(e, v)
         ev = jdoc['paths'][e][v]
-        
     defs = jdoc['definitions']
     point = defs['Point']
     ps = defs['PointsInAOUSearch']
     # TODO: leverage examples  in definitions.
 
 
+# TODO: mv with func below
+def make_endpoint(url, service):
+    eps = [e for (e,v) in service.ends]   # all endpoints for swagger
+    up = url['path']
+    for i, word in enumerate(up):
+        if is_version(word):
+            svc = up[:i+1]
+            endpoint = '/' + '/'.join(up[i+1:])
+            # check here to see if swagger endpoint contains version or not.
+            # Include the version if it is in the swagger file.
+            if endpoint not in eps:
+                for e in eps:
+                    if e.endswith(endpoint):
+                        endpoint = e
+    return endpoint
+
+
+# TODO: (almost) nothing specific to CRS here.
+# mv to somewhere
 def request_for_service(service):
     def do_pm_request(postman_request):
       try:
         pr = postman_request['request']
-        method = pr['method']
-        url = pr['url']
-
         if 'body' in pr:
             ctrb = pr['body']
     #        assert sorted(list(ctrb)) == ['mode', 'options', 'raw']
@@ -109,46 +124,34 @@ def request_for_service(service):
         else:
             bdecoded = ''
 
-        # get (endpoint, verb)
-        eps = [e for (e,v) in service.ends]   # all endpoints for swagger
-        up = url['path']
-        for i, word in enumerate(up):
-            if is_version(word):
-                svc = up[:i+1]
-                endpoint = '/' + '/'.join(up[i+1:])
-                # check here to see if swagger endpoint contains version or not.
-                # Include the version if it is in the swagger file.
-                if endpoint not in eps:
-                    # TODO: do something.
-                    for e in eps:
-                        if e.endswith(endpoint):
-                            endpoint = e
-            # TODO: above is good but has grow enough to warrant a function.
-        verb = method.lower()
-
-        # TODO:  insert template variables...     DONE
-        dpid = one_dict(pr['header'])['data-partition-id']
-        source = dict(data_partition_id='foo..dpi..bar')
-        subbed = insert_params(dpid, source)
-
-        # get params
+        url = pr['url']
         params = one_dict(url['query']) if 'query' in url else {}
         if bdecoded:
             params['body'] = bdecoded
-        valid_params = '???'
 
-        # TODO: validate postman data   DONE
+        endpoint = make_endpoint(pr['url'], service)
+        verb = pr['method'].lower()
+
+        # TODO: this stuff is hard-coded.
+        # Source will be an Environment.
+        # Targets will be anything in parameters or headers.
+        dpid = one_dict(pr['header'])['data-partition-id']
+        source = dict(data_partition_id='foo..dpi..bar')
+        subbed = insert_params(dpid, source)
+        # TODO: strategy...
+        # serialize params
+        # substitute
+        # deserialize params
+
         v = service._validator(endpoint, verb)
-        # TODO: this _validator is hard-coded to CRS Catalog.
-        # Do it for arbitrary OSDU service.
         schema = v.v.schema   # in case we want to have a look.
-
         valid_params = 'OK' if v.is_valid(params) else 'invalid params'
         if is_bad_schema(schema):
             valid_params = 'crap schema'
             assert endpoint, verb == ('/coordinate-reference-system', 'get')
             # TODO: this (endpoint, verb) has no useful schema.
             # What to do about it?
+        # TODO: call the endpoint with the params.
 
         print(postman_request['name'])
         print(endpoint, verb)
@@ -160,28 +163,7 @@ def request_for_service(service):
     return do_pm_request
 
 
-def show_contents(pmjdoc, *names):
-    """
-    show_contents(pmjdoc)
-    show_contents(pmjdoc, 'Core Services')
-    show_contents(pmjdoc, 'Core Services', 'Entitlements')
-    show_contents(pmjdoc, 'Core Services', 'CRS Catalog', 'V3')
-    """
-    pm_item = fetch_thing(pmjdoc, *names)
-    space = ' '
-    i = 0
-    indent = space * i
-    for name in names:
-        print(f'{indent}{name}')
-        i += 4
-        indent = space * i
-    if 'item' in pm_item:
-        for dct in pm_item['item']:
-            t = 'r' if 'request' in dct else 'i' 
-            print(f"{indent}{t} {dct['name']}")
-
-
-
+# TODO: mv  (to api_tools?)
 class Service:
     def __init__(self, call, _validator, ends):
         self.call = call
@@ -194,8 +176,7 @@ smap = {
 }
 
 
-
-def test_crs_catalog():
+def test_crs_catalog():   # and conversion too.
     global pmjdoc
     pmjdoc = parsed_file_or_url(pm_files()[0])
 
@@ -212,21 +193,16 @@ def test_crs_catalog():
     test_pm_section(pmjdoc, *names)
 
 
+# TODO: mv to ?
 def test_pm_section(pmjdoc, *names):
   try:
     """
     Grab arbitrary section from Postman file and do the PM requests.
-    # TODO: associate section of Postman file with an API client.
-    # So far I have two.
-    #
-    # TODO: and figure out how to deal with the irritating problem of version
-    # prefixed to the url for SOME services.
     """
     postman_item = fetch_thing(pmjdoc, *names)
     rnames = [thing['name'] for thing in postman_item['item']]
     service = smap[names[1]]
     do_pm_request = request_for_service(service)
-
     for name in rnames:
         noms = names + (name,)
         rf = do_pm_request(fetch_thing(pmjdoc, *noms))
@@ -252,4 +228,25 @@ Core Services
         i V3
         r Health Check
 """
+# TODO: mv to where fetch_thing is.
+def show_contents(pmjdoc, *names):
+    """
+    show_contents(pmjdoc)
+    show_contents(pmjdoc, 'Core Services')
+    show_contents(pmjdoc, 'Core Services', 'Entitlements')
+    show_contents(pmjdoc, 'Core Services', 'CRS Catalog', 'V3')
+    """
+    pm_item = fetch_thing(pmjdoc, *names)
+    space = ' '
+    i = 0
+    indent = space * i
+    for name in names:
+        print(f'{indent}{name}')
+        i += 4
+        indent = space * i
+    if 'item' in pm_item:
+        for dct in pm_item['item']:
+            t = 'r' if 'request' in dct else 'i' 
+            print(f"{indent}{t} {dct['name']}")
+
 
